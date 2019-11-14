b928bebb0dcf339ecbb15af6cc40eb66af37dde0
Nickolas Ashikawa <nashikaw@hawaii.edu>
2019-11-13 21:51:59 -1000

    Took code from smartphone products

--- Ashikawa_Nickolas_Assignment1/Public/products.js
+++ Ashikawa_Nickolas_Assignment1/Public/products.js
@@ -3,27 +3,27 @@ products = [
             products1 = {
             brand1 : "Apple Macbook",
             price1 : 1200.00,
-            image1 : "mbp13touch-space-select-201807.jpeg",
+            image1 : "/Users/nickolasashikawa/Documents/GitHub/ITM352_F19_Repository/Ashikawa_Nickolas_Assignment1/Public/images/mbp13touch-space-select-201807.jpeg",
         },
 
         //Products 2
             products2 = {
             brand2 : "Windows Surface Pro",
             price2 : 900.00,
-            image2 : "download.jpeg",
+            image2 : "/Users/nickolasashikawa/Documents/GitHub/ITM352_F19_Repository/Ashikawa_Nickolas_Assignment1/Public/images/download (1).jpeg",
         },
 
         //Products 3
             products3 = {
             brand3 : "Windows Dell",
             price3 : 1000.00,
-            image3 : "download (1).jpeg",
+            image3 : "/Users/nickolasashikawa/Documents/GitHub/ITM352_F19_Repository/Ashikawa_Nickolas_Assignment1/Public/images/download.jpeg",
         },
 
         //Products 4
             products4 = {
             brand4 : "Windows Surface Laptop",
             price4 : 950.00,
-            image4 : "download (2).jpeg",
+            image4 : "/Users/nickolasashikawa/Documents/GitHub/ITM352_F19_Repository/Ashikawa_Nickolas_Assignment1/Public/images/download (2).jpeg",
         },
 ];
