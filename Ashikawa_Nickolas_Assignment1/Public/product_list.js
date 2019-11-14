a1273483410e1918bbc0d5a37687a96f368f3605
Nickolas Ashikawa <nashikaw@hawaii.edu>
2019-11-14 01:59:01 -1000

    edits

--- Ashikawa_Nickolas_Assignment1/Public/product_list.js
+++ Ashikawa_Nickolas_Assignment1/Public/product_list.js
@@ -3,26 +3,27 @@ var products = [
         //Products 1 
             "product" : "Apple Macbook",
             "price" : 1200.00,
-            "image": "./Apple_Macbook.jpeg", 
+            "image": "./Apple_Macbook.jpeg"
 },
 {
         //Products 2 
             "product" : "Windows Surface Pro",
             "price" : 900.00,
-            "image" : "./Windows_Surface_Pro.jpeg",
+            "image" : "./Windows_Surface_Pro.jpeg"
 },
 {
         //Products 3 
             "product" : "Windows Dell",
             "price" : 1000.00,
-            "image" : "./Windows_Dell.jpeg",
+            "image" : "./Windows_Dell.jpeg"
 },
 {
         //Products 4 
             "product" : "Windows Surface Laptop",
             "price" : 950.00,
-            "image" : "./Windows_Surface_Laptop.jpeg",
+            "image" : "./Windows_Surface_Laptop.jpeg"
 },
+
 ];
 //Referred from Lab 13
 if(typeof module != 'undefined') {
