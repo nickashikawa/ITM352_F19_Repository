2957d1300edc911156e4803755d47f4b6f07c939
Nickolas Ashikawa <nashikaw@hawaii.edu>
2019-11-14 00:58:25 -1000

    Editing Assignment 1

--- Ashikawa_Nickolas_Assignment1/Public/product_data.js
+++ Ashikawa_Nickolas_Assignment1/Public/product_data.js
@@ -1,33 +1,30 @@
-var products = 
-[
-        //Products 1
-            products1 = {
-            "brand1" : "Apple Macbook",
-            "price1" : 1200.00,
-            "image1" : "/Users/nickolasashikawa/Documents/GitHub/ITM352_F19_Repository/Ashikawa_Nickolas_Assignment1/Public/images/Apple_Macbook.jpeg",
-        },
-
-        //Products 2
-            products2 = {
-            "brand2" : "Windows Surface Pro",
-            "price2" : 900.00,
-            "image2" : "./Windows_Surface_Pro.jpeg",
-        },
-
-        //Products 3
-            products3 = {
-            "brand3" : "Windows Dell",
-            "price3" : 1000.00,
-            "image3" : "./Windows_Dell.jpeg",
-        },
-
-        //Products 4
-            products4 = {
-            "brand4" : "Windows Surface Laptop",
-            "price4" : 950.00,
-            "image4" : "./Windows_Surface_Laptop.jpeg",
-        },
+var products = [
+{    
+        //Products 1 
+            "product" : "Apple Macbook",
+            "price" : 1200.00,
+            "image" : "/Users/nickolasashikawa/Documents/GitHub/ITM352_F19_Repository/Ashikawa_Nickolas_Assignment1/Public/images/Apple_Macbook.jpeg",
+},
+{
+        //Products 2 
+            "product" : "Windows Surface Pro",
+            "price" : 900.00,
+            "image" : "./Windows_Surface_Pro.jpeg",
+},
+{
+        //Products 3 
+            "product" : "Windows Dell",
+            "price" : 1000.00,
+            "image" : "./Windows_Dell.jpeg",
+},
+{
+        //Products 4 
+            "product" : "Windows Surface Laptop",
+            "price" : 950.00,
+            "image" : "./Windows_Surface_Laptop.jpeg",
+},
 ];
+//Referred from Lab 13
 if(typeof module != 'undefined') {
     module.exports.products = products;
   }
--- Ashikawa_Nickolas_Assignment1/server.js
+++ Ashikawa_Nickolas_Assignment1/server.js
@@ -1,37 +1,24 @@
-import express, { static } from 'express';
-import { urlencoded } from "body-parser";
+var data = require('./public/product_data.js'); //get the data from product_data.js
+var products = data.products;
 
+var express = require('express');
 var app = express();
+var myParser = require("body-parser");
+var data = require("./public/product_data.js");
+
 app.all('*', function (request, response, next) {
-    console.log(request.method + ' to ' + request.path);
+    console.log(request.method + ' to ' + request.path)
     next();
 });
 
-app.use(urlencoded({ extended: true }));
-app.post("/process_form", function(request, response) {
-    let POST = request.body;
-    if (typeof POST['quantity_textbox'] != 'undefined'){
-        displayPurchase(response);
-        
-    }
+app.use(myParser.urlencoded({ extended: true }));
+//to process the response from what is typed in the form
+app.post("/process_form", function (request, response) {
+   let POST = request.body;
+   if (typeof POST['quantity_textbox'] != 'undefined') {
+    displayPurchase(POST, response);
+    
+} 
 });
-
-app.use(static('./public'));
+app.use(express.static('./public'));
 app.listen(8080, () => console.log(`listening on port 8080`));
-
-function isNonNegInt(q, returnErrors = false) {
-    errors = []; // assume no errors at first
-    if(Number(q) != q) errors.push('Not a number'); // Check if string is a number value
-    if(q < 0) errors.push(' Negative value'); // Check if it is non-negative
-    if(parseInt(q) != q) errors.push(' Not an integer'); // Check that it is an integer
-    return returnErrors ? errors : (errors.length == 0); 
-}
-
-function displayPurchase(POST, response){
-    q = POST['quantity_textbox'];
-    if(isNonNegInt(q)) {
-        response.send(`Thank you for purchasing ${q} things!`);
-     } else {
-        response.send(`${q} is not a quantity! Press the back button and try again.`);
-     }
-}
