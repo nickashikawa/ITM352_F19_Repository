f6f8b145c61faebab39a4a9bf0de15699be543ba
Nickolas Ashikawa <nashikaw@hawaii.edu>
2019-11-13 14:06:22 -1000

    Editing Assignment 1

--- Ashikawa_Nickolas_Assignment1/Public/products.js
+++ Ashikawa_Nickolas_Assignment1/Public/products.js
@@ -1,33 +1,33 @@
 products = [
-           //Products 1
+        //Products 1
             products1 = {
-            brand1 = "Apple Macbook",
-            price1 = 1200.00,
-            image1 = "mbp13touch-space-select-201807.jpeg",
+            brand1 : "Apple Macbook",
+            price1 : 1200.00,
+            image1 : "mbp13touch-space-select-201807.jpeg",
         },
         products.push(products1),
 
         //Products 2
             products2 = {
-            brand2 = "Windows Surface Pro",
-            price2 = 900.00,
-            image2 = "download.jpeg",
+            brand2 : "Windows Surface Pro",
+            price2 : 900.00,
+            image2 : "download.jpeg",
         },
         products.push(products2),
 
         //Products 3
             products3 = {
-            brand3 = "Windows Dell",
-            price3 = 1000.00,
-            image3 = "download (1).jpeg",
+            brand3 : "Windows Dell",
+            price3 : 1000.00,
+            image3 : "download (1).jpeg",
         },
         products.push(products3),
 
         //Products 4
             products4 = {
-            brand4 = "Windows Surface Laptop",
-            price4 = 950.00,
-            image4 = "download (2).jpeg",
+            brand4 : "Windows Surface Laptop",
+            price4 : 950.00,
+            image4 : "download (2).jpeg",
         },
         products.push(products4),
 ];
