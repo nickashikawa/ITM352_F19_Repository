ec75712674305d21bf1ad2d72f3edccecd3bc8bd
Nickolas Ashikawa <nashikaw@hawaii.edu>
2019-11-13 17:25:22 -1000

    Editing Assignment one using inspect

--- Ashikawa_Nickolas_Assignment1/Public/products.js
+++ Ashikawa_Nickolas_Assignment1/Public/products.js
@@ -5,7 +5,6 @@ products = [
             price1 : 1200.00,
             image1 : "mbp13touch-space-select-201807.jpeg",
         },
-        products.push(products1),
 
         //Products 2
             products2 = {
@@ -13,7 +12,6 @@ products = [
             price2 : 900.00,
             image2 : "download.jpeg",
         },
-        products.push(products2),
 
         //Products 3
             products3 = {
@@ -21,7 +19,6 @@ products = [
             price3 : 1000.00,
             image3 : "download (1).jpeg",
         },
-        products.push(products3),
 
         //Products 4
             products4 = {
@@ -29,5 +26,4 @@ products = [
             price4 : 950.00,
             image4 : "download (2).jpeg",
         },
-        products.push(products4),
 ];
