b9dd7f6caacf99a31948d7e8d98a61c6d22ea61f
Nickolas Ashikawa <nashikaw@hawaii.edu>
2019-12-19 17:59:49 -1000

    design

--- Assignment4/Public/tetris.js
+++ Assignment4/Public/tetris.js
@@ -105,7 +105,7 @@ function drawMatrix(matrix, offset) {
 }
 
 function draw() {
-    context.fillStyle = '#FFF'; /*Changing the color inside the canva*/
+    context.fillStyle = '#282828'; /*Changing the color inside the canva*/
     context.fillRect(0, 0, canvas.width, canvas.height);
 
     drawMatrix(arena, {x: 0, y: 0});
